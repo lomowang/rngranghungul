a1f70501c3a6699ef16f673525591ede260b4528
lomowang <frees1102@gmail.com>
2023-12-24 16:00:28 +0800

    20231224

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,59 +1,43 @@
-import React, { useState } from "react";
-
 const Contact = () => {
-  const [email, setEmail] = useState(""); // 用于存储输入的电子邮件地址
-
-  const handleSubmit = (event) => {
-    event.preventDefault(); // 阻止表单的默认提交行为
-    // 在这里处理电子邮件发送逻辑
-    console.log("Submitted email:", email);
-    // 可以在这里调用 API 发送电子邮件或执行其他操作
-  };
-
   return (
-    <section>
-      <div>
-        <h2>聯絡我們</h2>
-        <p>Contact</p>
+    <section className="py-8">
+      <div className="flex flex-col items-center">
+        <h2 className="text-2xl font-bold mb-4">聯絡我們</h2>
+        <p className="text-sm">Contact</p>
       </div>
+      <div className="max-w-4xl mx-auto px-4 md:px-8 bg-gray-50">
+        <div className="md:flex md:justify-around md:items-start text-justify">
+          <div className="md:w-1/2 mb-8 m-8 ">
+            <h2 className="text-xl font-bold">山貳六影像工作室</h2>
+            <h3 className="text-lg">Mount Two-Six Studio</h3>
+            <p className="my-4 text-sm">
+              見山三階段，是山、不是山、又是山。「山」作為自我的表示，從「山」出發的觀看方式，2010至今，使用影像媒材作為觀察與參與式的實驗創作發想。
+            </p>
+            <p className="my-4 text-sm">
+              “Seeing the Mountain in Three Stages" refers to the concept of the
+              mountain: it is a mountain, it is not a mountain, and yet it is a
+              mountain again...
+            </p>
+          </div>
 
-      <div>
-        <div>
-          <h2>山貳六影像工作室</h2>
-          <h3>Mount Two-Six Studio</h3>
-          <p>
-            見山三階段，是山、不是山、又是山。「山」作為自我的表示，從「山」出發的觀看方式，2010至今，使用影像媒材作為觀察與參與式的實驗創作發想。
-          </p>
-          <p>
-            “Seeing the Mountain in Three Stages" refers to the concept of the
-            mountain: it is a mountain, it is not a mountain, and yet it is a
-            mountain again. The term “mountain" represents the self and
-            signifies a way of perceiving. From this perspective, since 2010
-            until now, the use of visual media has been employed as a means of
-            experimental artistic creation, allowing for observation and
-            participatory ideas.
-          </p>
-          <p>
-            Rngrang．Hungul 歡迎大家邀她到世界各地放映她的作品或演講。
-            如果你想聊聊拍攝主題、書籍、美酒或其他事物，歡迎隨時聯絡。
-          </p>
-          <p>E-mail：rainbowandlois@gmail.com</p>
+          <div className="md:w-1/2 mb-8 m-8 text-sm">
+            <p className="mt-8">Rngrang．Hungul</p>
+            <p className="mt-2">
+              歡迎大家邀她到世界各地放映她的作品或演講。
+              如果你想聊聊拍攝主題、書籍、美酒或其他事物，歡迎隨時聯絡。
+            </p>
+            <p className="my-4 ">
+              Everyone is welcome to invite her to screen her works or speak at
+              locations around the world...
+            </p>
+            <p className="text-sm font-bold">
+              E-mail:{" "}
+              <a href="mailto:rainbowandlois@gmail.com" className="underline">
+                rainbowandlois@gmail.com
+              </a>
+            </p>
+          </div>
         </div>
-        <di>
-          <h3>發表迴響</h3>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="email">電子郵件:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <button type="submit">提交</button>
-          </form>
-        </di>
       </div>
     </section>
   );
